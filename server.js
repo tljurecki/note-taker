570997678bd07e486aa637b9fb56bc3fb7bdd8a2
Tanya Jurecki <tljurecki@gmail.com>
2022-05-03 14:31:10 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+//dependencies
+const express = require('express');
+const path = require('path');
+
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//create server
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
