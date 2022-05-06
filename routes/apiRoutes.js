11c0e7376e8aa26ee036575bbdf789f17c460305
Tanya Jurecki <tljurecki@gmail.com>
2022-05-06 13:46:32 -0400

    added comments to code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 //import unique id from npm
 const uniqid = require('uniqid');
 
+//write notes to db.json
 function saveToDb (notes) {
     notes = JSON.stringify(notes);
     fs.writeFileSync('./db/db.json', notes, (err) => {
@@ -21,33 +22,35 @@ router.get('/notes', (req, res) => {
 //Post to add notes
 router.post('/notes', (req, res) => {
     const addNote = req.body;
-    
+    //give each new note a unique id
     addNote.id = uniqid();
-
+    //read data from db.json
    let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     //push note in 'db.json'
     savedNotes.push(addNote);
 
     fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
-
+    //send notes to function to save to db.json
     saveToDb(savedNotes);
     res.json(savedNotes);
    
 });
 
+//delete items
 router.delete('/notes/:id', (req, res) => {
     //get id of note to be deleted
     const noteId = req.params.id.toString();
     console.log(noteId);
     let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+   //check for matching id in notes array
     for(i = 0; i < savedNotes.length; i++) {
         if (savedNotes[i].id == noteId);
         res.send(savedNotes[i]);
-        
+        //removes the deleted note
         savedNotes.splice(i, 1);
         break;
     }
-
+    //write the rest of the files to db.json
     saveToDb(savedNotes);
 ;
 })
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const path = require('path');
-
+//for blank api calls send to homepage
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
-
+//for /notes, send to notes page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
-
+//default send to main page
 router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
