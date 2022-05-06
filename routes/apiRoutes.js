23c1fbb61869dab4233afab024ccdeabf794f36e
Tanya Jurecki <tljurecki@gmail.com>
2022-05-06 12:37:34 -0400

    fixed html routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const fs = require('fs');
 //import unique id from npm
 const uniqid = require('uniqid');
-const savedNotes = require('../db/db.json');
+
 
 
 //Get all saved notes
 router.get('/notes', (req, res) => {
-    savedNotes = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+    savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     res.json(savedNotes);
 });
 
@@ -17,11 +17,11 @@ router.post('/notes', (req, res) => {
     
     addNote.id = uniqid();
 
-    savedNotes = JSON.parse(fs.readFileSync('../db/db.json', 'utf8'));
+    savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     //push note in 'db.json'
     savedNotes.push(addNote);
 
-    fs.writeFileSync('../db/db.json', JSON.stringify(savedNotes));
+    fs.writeFileSync('./db/db.json', JSON.stringify(savedNotes));
 
     res.json(savedNotes);
    
